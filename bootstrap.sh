85ec757f61c1a53a57da86024d20bfe511960ef3
Chakrit Tansakul <coad4u4ever@appleprontos-Mac-mini-3.local>
2015-06-03 14:21:54 +0700

    add installation steps to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,16 @@
 su vagrant <<'EOF'
-
+sudo apt-get update
+sudo apt-get install python-setuptools
+sudo easy_install pip
+sudo pip install virtualenv virtualenvwrapper
+echo 'export WORKON_HOME=$HOME/.virtualenvs' >> ~/.bash_profile
+echo 'source /usr/local/bin/virtualenvwrapper.sh' >> ~/.bash_profile
+echo 'export PIP_VIRTUALENV_BASE=$WORKON_HOME' >> ~/.bash_profile
+source ~/.bash_profile
+mkvirtualenv django-contact-form
+pip install Django==1.6.2
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable --ruby
+source /home/vagrant/.rvm/scripts/rvm
+gem install --no-rdoc --no-ri cucumber rspec selenium-webdriver headless chronic capybara
 EOF
