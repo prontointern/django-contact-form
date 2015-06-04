a5078878fcc790b6669767915bbba9e083a34f3a
Chakrit Tansakul <coad4u4ever@appleprontos-Mac-mini-2.local>
2015-06-04 13:35:35 +0700

    add header in contact form

--- django_contact_form_project/contacts/tests/test_views.py
+++ django_contact_form_project/contacts/tests/test_views.py
@@ -2,10 +2,14 @@ from django.test import TestCase
 
 
 class ContactViewTest(TestCase):
-    def test_contactView_is_accessable(self):
+    def test_contact_view_is_accessible(self):
         url = '/contact/'
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
-
+    def test_contact_view_should_have_contact_form_header(self):
+        url = '/contact/'
+        response = self.client.get(url)
+        expected =  '<h1>Contact Form</h1>'
+        self.assertContains(response, expected, status_code=200)
 
--- django_contact_form_project/contacts/views.py
+++ django_contact_form_project/contacts/views.py
@@ -4,3 +4,14 @@ from django.views.generic import TemplateView
 
 class ContactView(TemplateView):
     template_name = 'contact_form.html'
+
+    def get(self, request):
+        header = 'Contact Form'
+        return render(
+            request,
+            self.template_name,
+            {
+                'header': header
+            }
+        )
+
