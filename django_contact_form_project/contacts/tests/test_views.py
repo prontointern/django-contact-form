eccafd9d417544a0a8ab358351bc48993e433012
Chakrit Tansakul <coad4u4ever@appleprontos-Mac-mini-3.local>
2015-06-04 13:58:30 +0700

    add form tag

--- django_contact_form_project/contacts/tests/test_views.py
+++ django_contact_form_project/contacts/tests/test_views.py
@@ -1,15 +1,18 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 
 class ContactViewTest(TestCase):
+    def setUp(self):
+        url = reverse('contact')
+        self.response = self.client.get(url)
+
     def test_contact_view_is_accessible(self):
-        url = '/contact/'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
-
+        self.assertEqual(self.response.status_code, 200)
     def test_contact_view_should_have_contact_form_header(self):
-        url = '/contact/'
-        response = self.client.get(url)
         expected =  '<h1>Contact Form</h1>'
-        self.assertContains(response, expected, status_code=200)
+        self.assertContains(self.response, expected, status_code=200)
 
+    def test_contact_view_should_have_form_tag(self):
+        expected = '<form action="." method="post">'
+        self.assertContains(self.response, expected, status_code=200)
